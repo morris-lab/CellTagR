f53de19caaf701dd0dd9b9e408b90f981fd07786
KaetheKong <kaethekung@gmail.com>
2019-06-11 10:28:19 -0500

    clarify documentation

--- R/CreateCellTagObject.R
+++ R/CreateCellTagObject.R
@@ -2,7 +2,7 @@
 #'
 #' This function creates a CellTag object that contains the basic information required for the object
 #' @param object.name The name of the object
-#' @param fastq.bam.input The input fastq/bam data directory
+#' @param fastq.bam.input The input fastq/bam data file path
 #' @param celltag.version Which version of CellTags are you working with?
 #' @return A CellTag Object with open attributes that can be filled as analysis moving along
 #' @keywords single-cell RNA-seq data, CellTagging
